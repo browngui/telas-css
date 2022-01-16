aa14f6afa991868eab568144a785e80b29217552
Guilherme Silva <87917952+browngui@users.noreply.github.com>
2022-01-16 19:29:41 -0300

    Delete luiz felipe directory

--- luiz felipe/DOM/ContDOM/script.js
+++
@@ -1,8 +0,0 @@
-let item1 = document.querySelector(`h1`);
-item1.addEventListener(`click`, mudarTema);
-
-function mudarTema(){
-    let body = document.body;
-    body.classList.toggle(`dark_mode`);
-}
-
--- luiz felipe/novo/rgb.js
+++
@@ -1,6 +0,0 @@
-let red = document.querySelector("#red");
-let green = document.querySelector("#green");
-let blue = document.querySelector("#blue");
-
-let div = document.querySelector(".cor");
-div.style.background = `rgb(${red}, ${green}, ${blue})`;
